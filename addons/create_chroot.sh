335f75c05bbe2231de6dafb4c6bf768926ee8276
Thierry Laurion <tlaurion@inverse.ca>
2016-09-09 15:49:27 -0400

    WIP: create_chroot manquait var/lock et var/cache (erreurs parceque non existants, reste à voir s'ils ont besoin d'être bindés). Mais je ne suis pas sûr que des mount -o bind soient ce que nous voulions pour tous les répertoires actuellement bindés. Par exemple, binder /etc au répertoire dans le chroot fait en sorte que le resolv.conf utilisé est le même que dans le host. Le copier par dessus remplace aussi le fichier du host dans ce cas. En fait, il faudrait voir les fichiers qui sont utilisés et modifiés et les copier manuellement, et binder les répertoires de binaires et librairies en RO.

--- addons/create_chroot.sh
+++ addons/create_chroot.sh
@@ -3,11 +3,11 @@ NS=$1
 BASE=$2
 
 if [ -z "$NS" ] || [ -z "$BASE" ]; then
-    echo "Missing parameter"
+    echo "Missing parameters. First is Network Namespace second is chroot base directory."
     exit 1;
 fi
 
-DIRS=(proc var etc lib lib64 usr sbin bin var/cache sys var/lib/samba dev tmp run/samba var/log/samba var/lock)
+DIRS=(proc var etc lib lib64 usr sbin bin var/cache sys var/lib/samba dev tmp run/samba var/log/samba var/lock var/cache)
 
 for dir in "${DIRS[@]}"; do
   [ -d $BASE/$NS/$dir ]                || mkdir -p $BASE/$NS/$dir
@@ -16,14 +16,16 @@ done
 touch /var/cache/samba$NS/secrets.tdb
 cp -fr /var/cache/samba$NS $BASE/$NS/var/cache/
 
-DIRS=(proc etc lib lib64 bin usr sbin sys var/lib/samba dev var/log/samba var/lock)
+DIRS=(proc etc lib lib64 bin usr sbin sys var/lib/samba dev var/log/samba var/lock var/cache)
 
 MOUNTS=(`mount | awk '{print $3}'`)
 
 for dir in "${DIRS[@]}"; do
   value=$BASE/$NS/$dir
-  if [[ ! " ${MOUNTS[@]} " =~ " ${value} " ]]; then
-    mount -o bind /$dir           $BASE/$NS/$dir
+  if [[ ! " ${MOUNTS[@]} " =~ " ${value} " ]]; then  
+    mount -o bind /$dir           $value
   fi
 done
+
+cp /etc/netns/$NS/resolv.conf $BASE/$NS/etc/resolv.conf
 
